5806249ade608166259e4718fb0d22d691f0e424
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-10-31 18:50:25 +0100

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -14,6 +14,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beer', { beers })
+      // console.log(beers[0].name)
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 
