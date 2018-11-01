9e36ab9347593ca4745f2fe20b6fd2cdc3069398
Emina Sonnad <esonnad@gmail.com>
2018-11-01 14:41:56 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,11 +11,23 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/random-beers', (req, res, next) => {
+  res.render('random');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {  
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
 app.listen(3000);
